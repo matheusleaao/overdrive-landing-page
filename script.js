4d87121dd25b6177ffd39db5034424812836234a
matheusleaao <mathegiov@hotmail.com>
2024-07-03 20:59:44 -0300

    modificações da página

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    const username = document.getElementById('username').value;
+    //const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const loginContainer = document.getElementById('login-container');
 
-    if (username === 'overdrive' && password === 'overdrive') { // Substitua pelos valores desejados
+    if (password === 'overdrive') { // Substitua pelos valores desejados
         window.location.href = './pagina-secreta/index.html'; // Substitua pela URL desejada
     } else {
         loginContainer.classList.remove('shake'); // Remova a classe antes de adicioná-la novamente
